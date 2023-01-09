17cae8f29fcdf85ad2f715660d8bad9b18d684b6
UmmuhanKavas <ummuhankavas@gmail.com>
2023-01-09 10:06:02 +0100

    authContextProvider updated

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,19 +1,28 @@
-import {useContext,createContext} from 'react';
-import {auth, googleProvider } from '../utils/firebase';
+import React, { useEffect } from 'react';
+// import { useContext } from 'react';
+import { useState } from 'react';
+import { createContext } from 'react';
+import { userObserver } from '../auth/firebase';
 
-//!Create context for authentication data
-const AuthContext = createContext();
+export const AuthContext = createContext();
+//* with custom hook
+// export const useAuthContext = () => {
+//   return useContext(AuthContext)
+// }
 
-//!Define a function to get data from Auth context
-export function useAuth(){
-    return useContext(AuthContext);
-}
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
 
-const AuthContextProvider = () => {
-  const {currentUser, setCurrentUser} = useState()
-    function signup()
-  return 
-    
-}
+  useEffect(() => {
+    // setCurrentUser(JSON.parse(sessionStorage.getItem('user')));
+    userObserver(setCurrentUser);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContextProvider;
